b519a28590c042bdb9aa4d0fa07c5317cd03393a
Rafael Francisco <broncasrafa@gmail.com>
2021-06-23 19:39:07 -0300

    adding

---
+++ 4. apis-and-microservices-projects/1. Timestamp Microservice/server.js
@@ -0,0 +1,67 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+app.get('/api/:date?', (req, res) => {
+  var value = req.params.date;    
+
+  // data vazia retorna data atual
+  if (value == undefined || value == '') {
+    var date = new Date();
+    return res.json({ unix: date.getTime(), utc: date.toUTCString() });
+  }
+
+  var unixResult = 0;
+  var utcResult = '';
+
+  if (value.includes('-')) {
+    var isValid = (new Date(value)).getTime() > 0;
+    if (isValid) {
+      var parts = value.split('-');
+      var date = new Date(parts[0], parts[1]-1, parts[2]); 
+      unixResult = date.getTime(); 
+      utcResult = date.toUTCString();     
+    } else {
+      return res.json({ error : "Invalid Date" });
+    }
+  } else {
+    var isValid = (new Date(parseInt(value))).getTime() > 0;
+    if (isValid) {
+      unixResult = parseInt(value);
+      utcResult = new Date(parseInt(value)).toUTCString();
+    } else {
+      return res.json({ error : "Invalid Date" });
+    }    
+  }
+
+  return res.json({ unix: unixResult, utc: utcResult });
+});
+
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
