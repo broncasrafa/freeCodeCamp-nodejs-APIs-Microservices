18d04f9ded4f2e54d6472f8c6aa904f28853c4e9
Rafael Francisco <broncasrafa@gmail.com>
2021-06-22 23:21:15 -0300

    adding mongodb and mongoose exercises file .js

--- 3. mongodb-and-mongoose/README.md
+++ 3. mongodb-and-mongoose/README.md
@@ -1,1 +1,21 @@
 # [MongoDB and Mongoose Challenges](https://www.freecodecamp.org/learn/apis-and-microservices/mongodb-and-mongoose/)
+
+## MongoDB and Mongoose
+
+**MongoDB** is a database application that stores JSON documents (or records) that you can use in your application. Unlike SQL, another type of database, Mongo is a non-relational or "NoSQL" database. This means Mongo stores all associated data within one record, instead of storing it across many preset tables as in a SQL database.
+
+**Mongoose** is a popular npm package that is often installed alongside Mongo. With Mongoose, you can use plain JavaScript objects instead of JSON, which makes it easier to work with Mongo. Also, it allows you to create blueprints for your documents called schemas, so you don't accidentally save the wrong type of data and cause bugs later.
+
+In the MongoDB and Mongoose courses, I learned the fundamentals of working with persistent data including how to set up a model, and save, delete, and find documents in the database.
+
+
+
+## ⚡ Technologies
+
+![Expressjs](https://img.shields.io/badge/Express.js-000000?style=flat-square&logo=express&logoColor=white)
+![Nodejs](https://img.shields.io/badge/-Nodejs-339933?style=flat-square&logo=Node.js&logoColor=white)
+![JavaScript](https://img.shields.io/badge/-JavaScript-black?style=flat-square&logo=javascript)
+![MongoDB](https://img.shields.io/badge/-MongoDB-black?style=flat-square&logo=mongodb)
+![Mongoose](https://img.shields.io/badge/MongoDB-4EA94B?style=flat-square&logo=mongodb&logoColor=white)
+![GitHub](https://img.shields.io/badge/-GitHub-181717?style=flat-square&logo=github)
+![npm](https://img.shields.io/badge/npm-CB3837?style=flat-square&logo=npm&logoColor=white)
--- 3. mongodb-and-mongoose/myApp.js
+++ 3. mongodb-and-mongoose/myApp.js
@@ -1,59 +1,126 @@
 require('dotenv').config();
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+mongoose.connect(process.env['MONGO_URI'], {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const personSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  age: Number,
+  favoriteFoods: [String]
+});
 
 
-let Person;
+let Person = mongoose.model('Person', personSchema);
 
 const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+  var person = new Person({name: "Rafael Francisco", age: 37, favoriteFoods: ['pizza', 'hotdog', 'ppk']});
+
+  person.save(function(err, data) {
+    if (err) 
+      return console.error(err);
+
+    done(null, data)
+  });
 };
 
-const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+var arrayOfPeople = [
+    {name: "Mirella Mansur", age: 26, favoriteFoods: ['pizza', 'hamburger', 'hotdog', 'miojo']},
+    {name: "Mia Khalifa", age: 20, favoriteFoods: ['apple', 'hamburger', 'strawberry']},
+    {name: "Piper Perri", age: 22, favoriteFoods: ['beringela', 'baguete', 'sausage']},
+    {name: "Lana Rhoades", age: 28, favoriteFoods: ['fastfood', 'chocolate']},
+  ];
+const createManyPeople = (arrayOfPeople, done) => {  
+  Person.create(arrayOfPeople, function (err, people) {
+    if (err) return console.log(err);
+    done(null, people);
+  });
 };
+
 
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find({ name: personName }, function(err, docs) {
+    if (err) return console.log(err);
+    done(null, docs);
+  });  
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({ favoriteFoods: food }, function(err, docs) {
+    if (err) return console.log(err);
+    done(null, docs);
+  }); 
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById(personId, function(err, doc) {
+    if (err) return console.log(err);
+    done(null, doc);
+  })
 };
+
 
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
 
-  done(null /*, data*/);
+  Person.findById(personId, (err, person) => {
+    if (err) return console.log(err);
+
+    person.favoriteFoods.push(foodToAdd);
+    person.save((err, doc) => {
+      if (err) return console.log(err);
+      done(null, doc);
+    })
+  })  
 };
+
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
+  const conditions = { name: personName};
+  const updateTo = { age: ageToSet };
+  const options = { new: true};
 
-  done(null /*, data*/);
+  Person.findOneAndUpdate(conditions, updateTo, options, function(err, person) {
+    if (err) return console.log(err);
+    done(null, person);
+  })
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findByIdAndDelete(personId, function(err, person) {
+    if (err) return console.log(err);
+    done(null, person);
+  });
+  
 };
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-
-  done(null /*, data*/);
+  const conditions = { name: nameToRemove };
+  Person.deleteOne(conditions, function(err, person) {
+    if (err) return console.log(err);
+    done(null, person);
+  }); 
 };
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+  Person.find({ favoriteFoods: foodToSearch })
+        .sort({ favoriteFoods: -1 })
+        .limit(2)
+        .select({ name: 1, favoriteFoods: 1 })
+        .exec(function(err, data) {
+          if (err) return console.log(err);
+          done(null, data);
+        })
 };
-
-/** **Well Done !!**
-/* You completed these challenges, let's go celebrate !
- */
 
 //----- **DO NOT EDIT BELOW THIS LINE** ----------------------------------
 
